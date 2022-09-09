351ad26b7575a3e03caf983fff782f2370261f78
Anthony Segedi <aj.segedi@gmail.com>
2022-09-08 23:16:49 -0500

    Updated Comments and output

--- styleCheck.ps1
+++ styleCheck.ps1
@@ -1,22 +1,24 @@
+# Author: Anthony Segedi
 # NOTE: you will need to run the following line to allow scripts to run on your system (needs to be done in administrator mode)
 # Set-ExecutionPolicy: Remote-Signed
 
-# progDir: the program directory. If blank, it is prompted for
-# Should be src if I recall correctly
-$progDir = "src"
+# progDir: the java source files directory (typically src). If blank, it is prompted for
+$progDir = "./src"
+#jarPath: The path to the checkstyle.jar file
 $jarPath = "./checkstyle.jar"
-$userCmd = $progDir + "/*.java"
-# Style Checker:
+# userCmd: The command used to execute on all java files in the respective directory
+$userCmd = Join-Path -Path "$progDir" -ChildPath "/*.java"
 
 if ([string]::IsNullOrEmpty($progDir)) {
     $progDir = Read-Host -prompt "Enter the path to your java files"
 }
 
 if (-not (Test-Path -Path $progDir -PathType Container)) {
-    Write-Output "'$progDir' is not a directory"
+    Write-Output "'$progDir' is not a directory. Please fix the $$progDir variable / reenter a valid path"
     Return
 }
 
+# Style Checker:
 java -jar $jarPath -c cs310code.xml $userCmd
 # Comments Checker: 
 java -jar $jarPath -c cs310comments.xml $userCmd
